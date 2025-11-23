63a6ac630f59e7828be01ed74e35eddaaa83fdc3
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2025-11-23 16:27:14 +0530

    user register crud

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService:UsersService){}
+
+    @Get()
+    getUsers(){
+        return this.userService.findUsers()
+    }
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto){
+        return this.userService.createUser(createUserDto)
+    }
+
+    @Put(':id')
+    async updateUser(@Param('id', ParseIntPipe) id:number, @Body() updateUserDto:UpdateUserDto){
+        return await this.userService.updateUser(id, updateUserDto)
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id', ParseIntPipe) id:number){
+        return await this.userService.deleteUser(id)
+    }
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,4 @@
+export type TCreateUserParams = {
+    username:string;
+    password:string
+}
